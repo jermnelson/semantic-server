af7aaa73d2e32b8e4af57ca8d8b75ac3c14bec7e
Jeremy Nelson <jermnelson.gmail.com>
2014-03-18 17:42:13 -0600

    Troubleshooting adding works

--- ingesters/mods2ds.py
+++ ingesters/mods2ds.py
@@ -13,7 +13,7 @@ import xml.etree.ElementTree as etree
 import flask_bibframe.models as bf_models
 
 from bson import ObjectId
-from flask_schema_org.models import CreativeWork, Person, Organization
+from flask_schema_org.models import CreativeWork, Map, Organization, Person
 
 from catalog.mongo_datastore import generate_record_info
 
@@ -22,7 +22,30 @@ from rdflib import Namespace
 FEDORA_M_NS = Namespace('http://www.fedora.info/definitions/1/0/management/')
 MODS_NS = Namespace("http://www.loc.gov/mods/v3")
 
-def get_or_add_person(name, client):
+def __set_role__(role_term, object_id, output):
+    """Helper function for setting a role
+
+    Function takes a role term, ObjectId, and an output dictionary,
+    creates or adds the role to a list in output withe serialized ID of the
+    object.
+
+    Args:
+        role_term: String value of the role
+        object_id: ObjectId
+        output: Dictionary of values
+
+    Returns:
+        output: Dictionary of values with the role added
+    """
+    if role_term in output:
+        output[role_term].append(str(object_id))
+    else:
+        output[role_term] = [str(object_id)]
+    return output
+
+
+
+def get_or_add_person(name, client, record_constants):
     """Function retrieves a schema:Person or adds a new schema:Person from MODS
 
     This function assumes that the mods name/namePart text is in the format of
@@ -31,6 +54,7 @@ def get_or_add_person(name, client):
     Args:
         name: MODS name element
         client: Mongo DB Client
+        record_constants: Dictionary with source and message for record info
 
     Returns:
         ObjectId: Mongo DB ObjectId for the schema.org Person
@@ -40,87 +64,95 @@ def get_or_add_person(name, client):
     name_type = name.attrib.get('type')
     if not name_type.startswith('personal'):
         return
-    bf_person = bf_models.Person()
+    bf_person = bf_models.Person(recordInfo=generate_record_info(
+                                        record_constants.get('source'),
+                                        record_constants.get('msg')))
+
     nameParts = name.findall("{{{0}}}namePart".format(MODS_NS))
     if len(nameParts) == 0:
         return
     elif len(nameParts) == 1:
-        existing_person = schema_org.Person.find_one({"name": nameParts[0].text},
+        full_name = nameParts[0].text
+        if full_name is None:
+            return
+        name_list = [part.strip() for part in full_name.split(",") if part.find('editor') < 0]
+        if full_name.find('editor') > -1:
+            full_name = ', '.join(name_list)
+        if name_list[-1].find("."): # Removes middle initial
+            name_list[-1] = name_list[-1].split(" ")[0]
+        existing_person = schema_org.Person.find_one({"name": full_name},
                                                      {"_id": 1})
         if existing_person:
             return existing_person.get('_id')
-        name_list = [part.strip() for part in namePart.text.split(",") if part.find('editor') < 0]
     else:
-        name_list = []
+        name_list = ['', '']
         for part in nameParts:
             if part.attrib.get('type') == 'family':
-                name_list
-
-
-
-
+                name_list[0] = part.text
+            if part.attrib.get('type') == 'given':
+                name_list[1] = part.text
+        full_name = ', '.join(name_list)
     person = Person(givenName=name_list[-1],
                     familyName=name_list[0],
-                    name=namePart.text)
-    # Ugh about hard-coding these values, should come from config
+                    name=full_name)
     person.recordInfo = generate_record_info(
-                              u"CoCCC",
-                              u'From Colorado College MODS record')
+                            record_constants.get('source'),
+                            record_constants.get('msg'))
     person.sameAs = []
     person_id = schema_org.Person.insert(person.as_dict())
     bf_person.relatedTo = [str(person_id)]
+    bf_person.label = person.name
     bf_person_id = bibframe.Person.insert(bf_person.as_dict())
     schema_org.Person.update({"_id": person_id},
                              {"$push": {'sameAs': str(bf_person_id)}})
     return person_id
 
 
-def get_or_add_organization(name, client):
+def get_or_add_organization(name, client, record_constants):
     """Existing organization or adds new Organization
 
     Args:
-        name: MODS name element
+        name: Name string
         client: Mongo DB Client
+        record_constants: Dictionary of record constants
 
     Returns:
         ObjectId: Mongo DB ObjectId for the schema.org Person
     """
     schema_org = client.schema_org
     bibframe = client.bibframe
-    name_type = name.attrib.get('type')
-    if not name_type.startswith('corporate'):
-        return
-    namePart = name.find("{{{0}}}namePart".format(MODS_NS))
-    if namePart.text is None:
-        return
-    existing_org = schema_org.Organization.find_one({"name": namePart.text},
+    existing_org = schema_org.Organization.find_one({"name": name},
                                                     {"_id": 1})
     if existing_org:
         return existing_org.get("_id")
-    organization = Organization(name=namePart.text)
-    organization['recordInfo'] = generate_record_info(
-                                     u"CoCCC",
-                                     u'From Colorado College MODS record')
-
+    organization = Organization(name=name)
+    setattr(organization,
+            'recordInfo',
+            generate_record_info(
+                record_constants.get('source'),
+                record_constants.get('msg')))
     new_org = schema_org.Organization.insert(organization.as_dict())
     bf_organization = bf_models.Organization(
-        relatedTo=str(new_org.get('_id')),
-        label=namePart.text,
-        recordInfo=generate_record_info(
-            u"CoCCC",
-            u'From Colorado College MODS record'))
+        relatedTo=str(new_org),
+        label=name)
+    setattr(bf_organization,
+            'recordInfo',
+            generate_record_info(
+                record_constants.get('source'),
+                record_constants.get('msg')))
     bf_id = bibframe.Organization.insert(bf_organization.as_dict())
     schema_org.Organization.update({"_id": new_org},
-                                   {"sameAs": [str(bf_organization.get('_id'))]})
+                                   {"$set": {"sameAs": [str(bf_id)]}})
     return new_org
 
 
-def add_base(mods, client):
+def add_base(mods, client, record_constants):
     """Adds common elements from MODS to their schema.org counterparts
 
     Args:
         mods: MODS XML etree
         client: Mongo DB Client
+        record_constants: Dictionary with constant values for record creation
 
     Returns:
         dict: Dictionary of schema.org properties
@@ -128,19 +160,35 @@ def add_base(mods, client):
     schema_org = client.schema_org
     bibframe = client.bibframe
     instance = bf_models.Instance()
-    output = {}
+    output = generate_record_info(record_constants.get('source'),
+                record_constants.get('msg'))
     # Process MODS name
     for name in mods.findall("{{{0}}}name".format(MODS_NS)):
         name_type = name.attrib.get('type', None)
         role = name.find("{{{0}}}role/{{{0}}}roleTerm".format(MODS_NS))
         if name_type == 'personal':
-            person_id = get_or_add_person(name, client)
-            if person_id and role:
+            person_id = get_or_add_person(name, client, record_constants)
+            if person_id is not None and role is not None:
                 if role.text == 'creator':
-                    if 'creator' in output:
-                        output['creator'].append(str(person_id))
-                    else:
-                        output['creator'] = [str(person_id)]
+                    output = __set_role__('creator',
+                                           person_id,
+                                           output)
+                if role.text == 'contributor':
+                    output = __set_role__('contributor',
+                                          person_id,
+                                          output)
+
+        elif name_type == 'corporate':
+            corporate_name = name.find("{{{0}}}namePart".format(MODS_NS)).text
+            if corporate_name is None or len(corporate_name) < 1:
+                continue
+            org_id = get_or_add_organization(corporate_name,
+                        client,
+                        record_constants)
+            if role.text == 'contributor':
+                output = __set_role__('contributor',
+                                      object_id,
+                                      output)
     # Process MODS title
     title = mods.find("{{{0}}}titleInfo/{{{0}}}title".format(MODS_NS))
     if title is not None:
@@ -148,20 +196,13 @@ def add_base(mods, client):
     # Process MODS originInfo
     originInfo = mods.find("{{{0}}}originInfo".format(MODS_NS))
     publisher = originInfo.find("{{{0}}}publisher".format(MODS_NS))
-    if publisher and publisher.text:
-        existing_org = schema_org.Organization.find_one(
-                           {"name": publisher.text},
-                           {"_id": 1})
-        if existing_org:
-            output['publisher'] = str(existing_org.get("_id"))
-        else:
-            organization = Organization(publisher.text)
-            organization['recordInfo'] = generate_record_info(
-                                            u"CoCCC",
-                                            u'From Colorado College MODS record')
-            output['publisher'] = str(schema_org.Organization.insert(
-                                        organization.as_dict()))
-        output['copyrightHolder'] = [output['publisher'],]
+    if publisher is not None and publisher.text:
+        publisher_id = get_or_add_organization(publisher.text,
+                                               client,
+                                               record_constants)
+        if publisher_id:
+            output['publisher'] = str(publisher_id)
+            output['copyrightHolder'] = [output['publisher'],]
 
     dateIssued = originInfo.find("{{{0}}}dateIssued".format(MODS_NS))
     if dateIssued is not None:
@@ -182,22 +223,52 @@ def add_base(mods, client):
     return output
 
 
-
-def add_publication_issue(mods, client):
+def add_map(mods, client, record_constants):
     schema_org = client.schema_org
     bibframe = client.bibframe
-    base_mods = add_base(mods, client)
+    base_mods = add_base(mods, client, record_constants)
+    map_work = Map(**base_mods)
+    map_dict = map_work.as_dict()
+    map_dict['@type'] = 'Map'
+    map_id = schema_org.CreativeWork.insert(map_dict)
+    cartography = bf_models.Cartography(label=map_dict.get('name',
+                                                map_dict.get('headline')),
+                                        relatedTo=[str(map_id)])
+    setattr(cartography,
+            'recordInfo',
+            generate_record_info(
+                record_constants['source'],
+                record_constants['msg']))
+    cartography_id = bibframe.Work.insert(cartography.as_dict())
+    schema_org.CreativeWork.update({"_id": map_id},
+                                   {"$set": {'sameAs': [str(cartography_id)]}})
+
+    return map_id
+
+def add_publication_issue(mods, client, issue_number, record_constants):
+    schema_org = client.schema_org
+    bibframe = client.bibframe
+    base_mods = add_base(mods, client, record_constants)
     publication_issue = CreativeWork(**base_mods)
-    publication_issue.additionalType = 'PublicationIssue'
+    setattr(publication_issue, 'issueNumber', issue_number)
+    pub_issue_dict = publication_issue.as_dict()
+    pub_issue_dict['@type'] = 'PublicationIssue'
+    pub_issue_id = schema_org.CreativeWork.insert(pub_issue_dict)
 
-def add_publication_issue(mods, client):
+    return pub_issue_id
+
+def add_publication_volume(mods, client, volume, record_constants):
     schema_org = client.schema_org
     bibframe = client.bibframe
-    base_mods = add_base(mods, client)
+    base_mods = add_base(mods, client, record_constants)
     publication_volume = CreativeWork(**base_mods)
-    publication_volume.additionalType = 'PublicationVolume'
+    setattr(publication_volume, 'volumeNumber', volume)
+    pub_volume_dict = publication_volume.as_dict()
+    pub_volume_dict['@type'] = 'PublicationVolume'
+    pub_volume_id = schema_org.CreativeWork.insert(pub_volume_dict)
+    return pub_volume_id
 
-def add_periodical(mods, client):
+def add_periodical(mods, client, record_constants):
     """Takes a MODS etree and adds a Periodical
     (as proposed http://www.w3.org/community/schemabibex/) to the Mongo
     Datastore
@@ -208,15 +279,24 @@ def add_periodical(mods, client):
     Args:
         mods: MODS XML etree
         client: Mongo DB Client
+        record_constants: Dictionary of Record constants
 
     Returns:
         ObjectId: Mongo DB ObjectId for the schema.org Thesis
     """
     schema_org = client.schema_org
     bibframe = client.bibframe
-    base_mods = add_base(mods, client)
+    base_mods = add_base(mods, client, record_constants)
     periodical = CreativeWork(**base_mods)
-    periodical.additionalType = 'Periodical'
+    periodical_dict = periodical.as_dict()
+    periodical_dict['@type'] = 'Periodical'
+    periodical_id = schema_org.CreativeWork.insert(periodical_dict)
+    return periodical_id
+
+
+
+
+
 
 
 
@@ -265,24 +345,10 @@ def add_thesis(mods, client):
     for note in mods.findall("{{{0}}}note".format(MODS_NS)):
         if note.attrib['type'] == 'thesis' and attrib.get('displayLabel') == "Degree Name":
             bf_text.dissertationDegree = note.text
+    return thesis.save()
 
 
-def insert_mods(db, mods_xml, redis_ds):
-    """Inserts a MODS XML datastream to MongoDB schema_org and bibframe
-    collections.
 
-    Args:
-        db: Flask-MongoKit Database
-        mods_xml: Raw MODS XML
-        redis_ds: Redis Datastore
-
-    Returns:
-        None
-
-    Raises:
-        None
-    """
-    pass
 
 
 
