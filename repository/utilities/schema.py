b85eb2e714ad8b700850310c2d42305624661c2c
Jeremy Nelson <jermnelson.gmail.com>
2015-07-24 16:53:49 -0600

    Updated ingester for schema.org

--- repository/utilities/schema.py
+++ repository/utilities/schema.py
@@ -1,12 +1,23 @@
 __author__ = "Jeremy Nelson"
 
+
+import rdflib
 from .ingesters import GraphIngester
 
 class Ingester(GraphIngester):
 
     def __init__(self, **kwargs):
         super(Ingester, self).__init__(**kwargs)
-        self.fusuki_ds = "schema"
-    
+
     def __process_subject__(self, row):
-        subject, graph = row
+        subject, graph = row[0], row[1]
+        schema_type = graph.value(subject=subject, predicate=rdflib.RDF.type)
+        existing_uri = self.searcher.triplestore.__sameAs__(str(subject))
+        if existing_uri:
+            subject = rdflib.URIRef(existing_uri)
+        fedora_url, new_graph = self.__add_or_get_graph__(
+            subject=subject, 
+            graph=graph,
+            graph_type=schema_type)
+        subject_uri = rdflib.URIRef(fedora_url)
+        return subject_uri
